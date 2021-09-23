c02779da6a1db8fce6756ef6889ed439869ed17b
bakachan <63496788+bakachanbaby@users.noreply.github.com>
2021-09-23 19:10:13 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let header = document.querySelector('.header-2');
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
