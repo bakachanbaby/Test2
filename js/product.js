4159dafdf3070a13550d6eebfd2d382fbd86a7a6
bakachan <63496788+bakachanbaby@users.noreply.github.com>
2021-09-26 21:46:11 +0700

    update4

--- js/product.js
+++ js/product.js
@@ -1,45 +1,99 @@
 let products = [
     {
-        name: 'Sneaker Jelly',
+        name1: 'Sneaker Jelly Cam',
+        name2: 'Sneaker Jelly Xanh',
+        name3: 'Sneaker Jelly Hồng',
+        color1:'Cam',
+        color2:'Xanh',
+        color3:'Hồng',
         image1: 'images/cam_tt03033_1_2d7e149ea50e43fd93ac4224c30aff87_master-removebg-preview.png',
         image2: 'images/cam_tt03033_7_8c11e0ab86614e02b1cbb58f1aab58c0_master-removebg-preview.png',
+        image3:'images/xanh_tt03033_1_1f9c2da554034f7cbeb87e923f8d9103_master-removebg-preview.png',
+        image4:'images/xanh_tt03033_7_2e24a57ef34949828eef8f5ae2900c70_master-removebg-preview.png',
+        image5:'images/hong_tt03033_1_564545dc856a43149cce1d309e803638_master-removebg-preview.png',
+        image6:'images/hong_tt03033_7_9d91b67be2754e359882c653b0567017_master-removebg-preview.png',
         old_price: '695,000₫',
         curr_price: '500,000₫'
     },
     {
-        name: 'Sneakers Ocean Wave',
+        name1: 'Sneakers Ocean Wave Xanh Tím',
+        name2: 'Sneakers Ocean Wave Trắng',
+        name3: 'Sneakers Ocean Wave Xám Xanh',
+        color1:'Xanh Tím',
+        color2:'Trắng',
+        color3:'Xám Xanh',
         image1: 'images/xanh-tim_tt05007_1_36caf1953a0a4be4adb187c365aadb37_master-removebg-preview.png',
         image2: 'images/xanh-tim_tt05007_7_64d428e86c0444d6b812aec30e0a85b3_master-removebg-preview.png',
+        image3:'images/trang_tt05007_1_f6152fc995014654aef09b1110db50f2_master-removebg-preview.png',
+        image4:'images/trang_tt05007_7_659561f47c50438fa71a1768010f75c8_master-removebg-preview.png',
+        image5:'images/xam-xanh_tt05007_1_1f8d902773904d3badfcb7679d50f798_master-removebg-preview.png',
+        image6:'images/xam-xanh_tt05007_7_1007951bb82e4444ba68aea839d27cdd_master-removebg-preview.png',
         old_price: '795,000₫',
         curr_price: '600,000₫'
     },
     {
-        name: 'Giày Cao Gót Mũi Nhọn Gót Trụ',
-        image1: 'images/hong_cg05112_1_bf9fbd481606439dbb7e17f6e497c207_master-removebg-preview.png',
-        image2: 'images/hong_cg05112_7_b3f3eb3852fe49ffb0bb8b2d13588710_master-removebg-preview.png',
+        name1: 'Sneakers Rush Crush Hồng',
+        name2: 'Sneakers Rush Crush Xanh',
+        name3: 'Sneakers Rush Crush Đen',
+        color1:'Hồng',
+        color2:'Xanh',
+        color3:'Đen',
+        image1: 'images/hong_tt03034_1_6850b1bee75746c7882c9bd9b5e0eec1_master-removebg-preview.png',
+        image2: 'images/hong_tt03034_2_974e43aaf1224bb5852375c47cd83edb_master-removebg-preview.png',
+        image3:'images/xanh_tt03034_1_3a58ceed4ba142fc9b52ac4c3a1cfcfb_master-removebg-preview.png',
+        image4:'images/xanh_tt03034_7_bd58bb1a752f4e3c8b68ad9441d311d8_master-removebg-preview.png',
+        image5:'images/den_tt03034_1_1d7f5a1d6fb94337938bb923354e0168_master-removebg-preview.png',
+        image6:'images/den_tt03034_7_f69b8b5071f843278481510ca737d878_master-removebg-preview.png',
         old_price: '455,000₫',
         curr_price: '350,000₫'
     },
     {
-        name: 'Giày Cao Gót Mũi Nhọn Gắn Dây',
-        image1: 'images/kem_cg09145_1_18e2ab82832346f6982def001237f366_master-removebg-preview.png',
-        image2: 'images/kem_cg09145_7_521ab2e484984978a3cda77d9eadff50_master-removebg-preview.png',
+        name1: 'Giày Thể Thao Classic Stick Cam',
+        name2: 'Giày Thể Thao Classic Stick Hồng',
+        name3: 'Giày Thể Thao Classic Stick Trắng',
+        color1:'Cam',
+        color2:'Hồng',
+        color3:'Trắng',
+        image1: 'images/cam_tt03032_1_a71848887b9e4e8ab21bff425d14dac0_master-removebg-preview.png',
+        image2: 'images/cam_tt03032_7_ce4ca12da2d0485baba0e1e71b104d4d_master-removebg-preview.png',
+        image3:'images/hong_tt03032_1_f8325e262c7749a4899834a72e976b90_master-removebg-preview.png',
+        image4:'images/hong_tt03032_7_a7e0cc6eda024ad0b01e8658768f855e_master-removebg-preview.png',
+        image5:'images/trang_tt03032_1_c75247f50b7f4c65b9a91d1a137f7f9f_master-removebg-preview.png',
+        image6:'images/trang_tt03032_8_14cadf1723d14f879a539b1eea6904e8_master-removebg-preview.png',
         old_price: '495,000₫',
         curr_price: '350,000₫'
     },
     {
-        name: 'Giày Xăng Đan Quai Ngang',
-        image1: 'images/den_sd03012_1_16526aae8aa644f496a7c1a58bd43185_master-removebg-preview.png',
-        image2: 'images/den_sd03012_2_c00fd13009cd405fac33d2d63be0bf71_master-removebg-preview.png',
-        old_price: '390,000₫',
-        curr_price: '220,000₫'
+        name1: 'Trendy Illuminated Chunky Đen',
+        name2: 'Trendy Illuminated Chunky Trắng',
+        name3: 'Trendy Illuminated Chunky Xanh',
+        color1:'Đen',
+        color2:'Trắng',
+        color3:'Xanh',
+        image1: 'images/den_tt07002_1_183b66917dfb4e42965d04fc14fc50bf_master-removebg-preview.png',
+        image2: 'images/den_tt07002_7_6034d9bb14f747b3bc20bd1cad5370e2_master-removebg-preview.png',
+        image3:'images/xanh-bac-ha_tt07002_1_91d7890933744b3eb1f6b2b343d5e0fe_master-removebg-preview.png',
+        image4:'images/xanh-bac-ha_tt07002_7_d15c2835805d463f93dc7fb7345af7f4_master-removebg-preview.png',
+        image5:'images/xanh_tt07002_1_2a06d3e21adb43f08b0c94f4d9af5c1f_master-removebg-preview.png',
+        image6:'images/xanh_tt07002_7_95c9f6b1346d4b6f96be802a6ce59c4d_master-removebg-preview.png',
+        old_price: '795,000₫',
+        curr_price: '600,000₫'
     },
     {
-        name: 'Giày Xăng Đan Khóa Gài',
-        image1: 'images/hong_sd07068_1_2c475afe4dfb467d86cf817437e21e97_master-removebg-preview.png',
-        image2: 'images/hong_sd07068_7_37e8f6b628fb4da9af90543c266a4a77_master-removebg-preview.png',
-        old_price: '495,000₫',
-        curr_price: '350,000₫'
+        name1: 'Trendy Bridging Chunky Xanh',
+        name2: 'Trendy Bridging Chunky Trắng',
+        name3: 'Trendy Bridging Chunky Hồng',
+        color1:'Xanh',
+        color2:'Trắng',
+        color3:'Hồng',
+        image1: 'images/trang-xanh_tt07003_1_b70ec2b11a1840718b617efc828ac2b1_master-removebg-preview.png',
+        image2: 'images/trang-xanh_tt07003_7_0eb85a2bbb4e4cb7a3f6c9a3dbd6418a_master-removebg-preview.png',
+        image3:'images/xanh-bac-ha_tt07002_1_91d7890933744b3eb1f6b2b343d5e0fe_master-removebg-preview.png',
+        image4:'images/xanh-bac-ha_tt07002_7_d15c2835805d463f93dc7fb7345af7f4_master-removebg-preview.png',
+        image5:'images/xanh_tt07002_1_2a06d3e21adb43f08b0c94f4d9af5c1f_master-removebg-preview.png',
+        image6:'images/xanh_tt07002_7_95c9f6b1346d4b6f96be802a6ce59c4d_master-removebg-preview.png',
+        old_price: '795,000₫',
+        curr_price: '600,000₫'  
     },
     {
         name: 'Giày Mules Quai Khoá Trang Trí',
@@ -93,21 +147,30 @@ renderProducts = (products) => {
             <div class="col-4 col-md-6 col-sm-12">
                 <div class="product-card">
                     <div class="product-card-img">
-                        <img class= "img_1" src="${e.image1}" alt="">
+                        <img class = "img_1" src="${e.image1}" alt="">
                         <img class = "img_2" src="${e.image2}" alt="">
+                        <img class = "img_3" src="${e.image3}" alt="">
+                        <img class = "img_4" src="${e.image4}" alt="">
+                        <img class = "img_5" src="${e.image5}" alt="">
+                        <img class = "img_6" src="${e.image6}" alt="">
                     </div>
                     <div class="product-card-info">
                         <div class="product-btn">
-                            <button class="btn-flat btn-hover btn-shop-now">shop now</>
-                            <button class="btn-flat btn-hover btn-cart-add">
-                                <i class='bx bxs-cart-add'></i>
+                            <button class="btn-flat btn-hover btn-shop-now" id="show-now1">
+                                ${e.color1}
                             </button>
-                            <button class="btn-flat btn-hover btn-cart-add">
-                                <i class='bx bxs-heart'></i>
+                            <button class="btn-flat btn-hover btn-cart-add" id="show-now2">
+                                ${e.color2}
+                            </button>
+                            <button class="btn-flat btn-hover btn-cart-add" id="show-now3">
+                                ${e.color3}
                             </button>
                         </div>
                         <div class="product-card-name">
-                            ${e.name}
+                            <span class = "ten_1">${e.name1}</span>
+                            <span class = "ten_2">${e.name2}</span>
+                            <span class = "ten_3">${e.name3}</span>
+                            
                         </div>
                         <div class="product-card-price">
                             <span><del>${e.old_price}</del></span>
@@ -117,7 +180,7 @@ renderProducts = (products) => {
                     </div>
                     <div class="product-card-info product-p2">
                         <div class="product-btn">
-                            <button class="btn-flat btn-hover btn-shop-now">shop now</>
+                            <button class="btn-flat btn-hover btn-shop-now">shop now</button>
                             <button class="btn-flat btn-hover btn-cart-add">
                                 <i class='bx bxs-cart-add'></i>
                             </button>
@@ -145,14 +208,116 @@ document.querySelector('#filter-toggle').addEventListener('click', () => filter_
 document.querySelector('#filter-close').addEventListener('click', () => filter_col.classList.toggle('active'))
 
 
-let btn = document.querySelector('.btn-shop-now');
-let img1 = document.querySelector('.img_1')
-let img2 = document.querySelector('.img_2')
-btn.addEventListener('mousemove', function (event) {
-    img1.style.display = 'none';
-    img2.style.display = 'block';
-})
-btn.addEventListener('mouseout', function (event) {
-    img1.style.display = 'block';
-    img2.style.display = 'none';
-})
+let btn1 = document.querySelectorAll('#show-now1');
+for (let i = 0; i < btn1.length; i++) {
+    let img1 = document.querySelectorAll('.img_1')
+    let img2 = document.querySelectorAll('.img_2')
+    let img3 = document.querySelectorAll('.img_3')
+    let img4 = document.querySelectorAll('.img_4')
+    let img5 = document.querySelectorAll('.img_5')
+    let img6 = document.querySelectorAll('.img_6')
+    let ten1 = document.querySelectorAll('.ten_1')
+    let ten2 = document.querySelectorAll('.ten_2')
+    let ten3 = document.querySelectorAll('.ten_3')
+    btn1[i].addEventListener('mousemove', function (event) {
+
+        img1[i].style.display = 'none';
+        img3[i].style.display = 'none';
+        img4[i].style.display = 'none';
+        img5[i].style.display = 'none';
+        img6[i].style.display = 'none';
+        img2[i].style.display = 'block';
+        ten1[i].style.display = 'block';
+        ten2[i].style.display = 'none';
+        ten3[i].style.display = 'none';
+        
+    })
+    btn1[i].addEventListener('mouseout', function (event) {
+        img1[i].style.display = 'block';
+        img2[i].style.display = 'none';
+        img3[i].style.display = 'none';
+        img4[i].style.display = 'none';
+        img5[i].style.display = 'none';
+        img6[i].style.display = 'none';
+        ten1[i].style.display = 'block';
+        ten2[i].style.display = 'none';
+        ten3[i].style.display = 'none';
+    })
+}
+let btn2= document.querySelectorAll('#show-now2');
+for (let i = 0; i < btn2.length; i++) {
+    let img1 = document.querySelectorAll('.img_1')
+    let img2 = document.querySelectorAll('.img_2')
+    let img3 = document.querySelectorAll('.img_3')
+    let img4 = document.querySelectorAll('.img_4')
+    let img5 = document.querySelectorAll('.img_5')
+    let img6 = document.querySelectorAll('.img_6')
+    let ten1 = document.querySelectorAll('.ten_1')
+    let ten2 = document.querySelectorAll('.ten_2')
+    let ten3 = document.querySelectorAll('.ten_3')
+    btn2[i].addEventListener('mousemove', function (event) {
+
+        img1[i].style.display = 'none';
+        img3[i].style.display = 'none';
+        img4[i].style.display = 'block';
+        img5[i].style.display = 'none';
+        img6[i].style.display = 'none';
+        img2[i].style.display = 'none';
+        ten1[i].style.display = 'none';
+        ten2[i].style.display = 'block';
+        ten3[i].style.display = 'none';
+    })
+    btn2[i].addEventListener('mouseout', function (event) {
+        img1[i].style.display = 'none';
+        img2[i].style.display = 'none';
+        img3[i].style.display = 'block';
+        img4[i].style.display = 'none';
+        img5[i].style.display = 'none';
+        img6[i].style.display = 'none';
+        ten1[i].style.display = 'none';
+        ten2[i].style.display = 'block';
+        ten3[i].style.display = 'none';
+    })
+}
+let btn3 = document.querySelectorAll('#show-now3');
+for (let i = 0; i < btn3.length; i++) {
+    let img1 = document.querySelectorAll('.img_1')
+    let img2 = document.querySelectorAll('.img_2')
+    let img3 = document.querySelectorAll('.img_3')
+    let img4 = document.querySelectorAll('.img_4')
+    let img5 = document.querySelectorAll('.img_5')
+    let img6 = document.querySelectorAll('.img_6')
+    let ten1 = document.querySelectorAll('.ten_1')
+    let ten2 = document.querySelectorAll('.ten_2')
+    let ten3 = document.querySelectorAll('.ten_3')
+    btn3[i].addEventListener('mousemove', function (event) {
+
+        img1[i].style.display = 'none';
+        img3[i].style.display = 'none';
+        img4[i].style.display = 'none';
+        img5[i].style.display = 'none';
+        img6[i].style.display = 'block';
+        img2[i].style.display = 'none';
+        ten1[i].style.display = 'none';
+        ten2[i].style.display = 'none';
+        ten3[i].style.display = 'block';
+    })
+    btn3[i].addEventListener('mouseout', function (event) {
+        img1[i].style.display = 'none';
+        img2[i].style.display = 'none';
+        img3[i].style.display = 'none';
+        img4[i].style.display = 'none';
+        img5[i].style.display = 'block';
+        img6[i].style.display = 'none';
+        ten1[i].style.display = 'none';
+        ten2[i].style.display = 'none';
+        ten3[i].style.display = 'block';
+    })
+}
+
+
+
+
+// for(let i = 0; i < btn.length; i++){
+//     console.log(btn[i]);
+// }
